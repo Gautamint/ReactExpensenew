735598d3a521f4fddd19c5974f5a35b2f16a09b0
Gautamint <gautaminit@gmail.com>
2022-08-27 23:45:53 +0530

    changes saved

--- src/component/Expenses/Expenses.js
+++ src/component/Expenses/Expenses.js
@@ -4,6 +4,7 @@ import "./Expense.css";
 
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -15,6 +16,7 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
+
   return (
     <div>
       <Card className="expenses">
@@ -22,14 +24,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        <ExpensesList items = {filteredExpenses} />
       </Card>
     </div>
   );
